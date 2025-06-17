59e1ea8da955a694f39553b0eec95abc1dfff22b
kubrahatice <kubra_bkc@hotmail.com>
2025-06-18 00:31:31 +0300

    Proje dosyası ekle.

---
+++ cohort10-week1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string ad = "Hatice Kübra Bekce";
+        Console.WriteLine("Hatice Kubra Bekce");
+        Console.WriteLine("Patika eğitimleri sayesinde yazılım dünyasında yer edineceğime inandığım için bu programdayım!!");
+    }
+}
